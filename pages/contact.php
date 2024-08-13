449ea98a1a9fb3395b7e76a155b53b7a47e3104f
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-13 18:12:52 +0545

    Add contact

---
+++ pages/contact.php
@@ -0,0 +1,66 @@
+<?php include('../includes/header.php'); ?>
+
+<head>
+    <title>About - Chautari</title>
+    <link rel="stylesheet" href="../assets/css/default.css">
+    <link rel="stylesheet" href="../assets/css/index.css">
+    <style>
+        nav ul li:nth-child(3) a {
+            background-color: #f0f0f0;
+            padding: 8px 15px;
+            border-radius: 20px;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <h1>Contact us</h1>
+        
+        <div class="boxed-text">
+            <p>If you have any questions, need assistance, or want to learn more about how
+                Chautari can enhance your event discovery experience, we’re here to help!
+                Reach out to us using the contact details below, and a member of our team will
+                get back to you promptly.
+            <br>
+            <br>
+                
+                Whether you're an event organizer looking to expand
+                your audience or an attendee searching for the perfect event, Sano Chautari
+                Software Company Limited is ready to support you every step of the way.</p>
+                
+                <br>
+
+            <p><strong>Email:</strong> <a href="mailto:support@chautari.com">support@chautari.com</a></p>
+            <p><strong>Phone:</strong> <a href="tel:+1234567890">+123-456-7890</a></p>
+            <p><strong>Address:</strong> Exhibition Road, Kathmandu, Nepal</p>
+    
+            <p>We look forward to connecting with you!</p>
+        </div>
+
+        <style>
+            .boxed-text {
+                border: 1px solid #f0f0f0;
+                background-color: #fafafa;
+                max-width: 750px;
+                padding: 20px;
+                border-radius: 10px;
+                margin: 20px auto;
+            }
+
+
+            p {
+                line-height: 1.8;
+            }
+
+
+        </style>
+
+       
+    </main>
+
+</body>
+</html>
+
+
+
+<?php include('../includes/footer.php'); ?>
