fd0c66daec511320e44bd64a44862e87029d2ed6
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-13 20:10:41 +0545

    enhance login

--- pages/login.php
+++ pages/login.php
@@ -3,25 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="../assets/sign.css">
+    <title>Log in - Chautari</title>
+    <meta name="description" content="Struggling to discover fun and exciting events near you? Chautari brings the best local events tailored to you so you can join
+            local happenings with ease.">
+    <meta property="og:title" content="Chautari — Find out about events around you.">
+    <link rel="icon" type="image/x-icon" href="../assets/images/favicon.ico" sizes="any">
+
+    <link rel="stylesheet" href="../assets/css/default.css">
+    <link rel="stylesheet" href="../assets/css/sign.css">
 </head>
-<body>
-<?php include '../includes/header.php'; ?>
+<body class="centerBody">
 
-<main>
-    <h1>Login</h1>
-    <form action="../handlers/login_handler.php" method="post">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
+<main class="formCenter">
+    <img class="smallImageInForm" src="../assets/images/logo-32.svg" alt="" srcset="">
+    
+    <h1>Log in</h1>
+    
+    <p class="snippetSignInfo">New to Chautari? <a href="./signup.php">Sign up</a></p>
+    
+    <div class="formbody">
+        <form action="../handlers/login_handler.php" method="post">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required>
+            
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required>
+            
+            <br>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-
-        <button type="submit">Login</button>
-    </form>
+            <button type="submit">Log in</button>
+        </form>
+    </div>
 </main>
 
-<?php include '../includes/footer.php'; ?>
 </body>
 </html>
