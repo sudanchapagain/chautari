d443d9e2b2068f72c06514a102f9b0f4137a3843
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-13 12:31:56 +0545

    Add index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php include'includes/header.php'; ?>
+
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <main>
+        <h1>Hello World</h1>
+
+        <?php include'templates/home.php'; ?>
+
+    </main>
+</body>
+</html>
+
+<?php include'includes/footer.php'; ?>
