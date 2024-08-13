5766a6b244d7bd5a00da1bc1657018f56b867ce4
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-13 13:09:09 +0545

    add client side password matching

---
+++ assets/js/password_match.js
@@ -0,0 +1,9 @@
+function validateForm() {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm_password").value;
+    if (password !== confirmPassword) {
+        document.getElementById("passNoMatch").innerHTML = ("<br/>Passwords do not match.<br/>")
+        return false;
+    }
+    return true;
+}
