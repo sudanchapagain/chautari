7300733b22caa4d9ad39777539958edf50c43de2
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-13 18:12:39 +0545

    Add about

---
+++ pages/about.php
@@ -0,0 +1,84 @@
+<?php include('../includes/header.php'); ?>
+
+<head>
+    <title>About - Chautari</title>
+    <link rel="stylesheet" href="../assets/css/default.css">
+    <link rel="stylesheet" href="../assets/css/index.css">
+    <style>
+        nav ul li:nth-child(2) a {
+            background-color: #f0f0f0;
+            padding: 8px 15px;
+            border-radius: 20px;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <h1>About</h1>
+
+        
+        <div class="boxed-text">
+            <div class="about-us-extended-logo">
+                <img src="../assets/images/extended-logo.png" alt="extended logo">
+            </div>
+
+            <p>Sano Chautari Software Company Limited has crafted a specialized software solution called Chautari,
+                tailored to revolutionize how events are discovered and accessed. Chautari stands out as a unique
+                tool in the realm of event management, focusing on the often overlooked yet crucial aspect of event
+                discovery. By streamlining the process of connecting users with events that align with their interests,
+                Chautari ensures that both event organizers and attendees find the perfect match, enhancing the overall
+                event experience.</p>
+
+                <br>
+
+            <p>The software is designed to simplify the search and discovery phase, making it easier for users to
+                navigate through various events without being overwhelmed by irrelevant options. Chautari employs
+                filters on events based on location, and other parameters. This ensures that users are presented
+                with the most relevant events, saving them time and effort in their search. For event organizers,
+                Chautari offers a platform to reach a more targeted audience, thereby increasing the likelihood of
+                attracting genuinely interested participants.</p>
+
+                <br>
+
+            <p>What sets Chautari apart is its focus on the discovery process within the broader event management
+                spectrum. While many event management tools offer features like ticketing, scheduling, and more,
+                Chautari hones in on the initial and critical step of connecting the right people to the right events.
+                This targeted approach allows Chautari to excel in creating a more engaging and personalized
+                experience for users, fostering a vibrant event community that benefits both organizers and attendees alike.</p>
+        </div>
+
+        <style>
+            .boxed-text {
+                border: 1px solid #f0f0f0;
+                background-color: #fafafa;
+                max-width: 750px;
+                padding: 20px;
+                border-radius: 10px;
+                margin: 20px auto;
+            }
+
+            .about-us-extended-logo {
+                margin: 30px auto;
+            }
+
+            .about-us-extended-logo img {
+                max-width: 500px;
+                display: block;
+                margin: 0 auto;
+            }
+
+            p {
+                line-height: 1.8;
+            }
+
+        </style>
+
+       
+    </main>
+
+</body>
+</html>
+
+
+
+<?php include('../includes/footer.php'); ?>
