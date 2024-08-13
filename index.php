385380403645f315dcd94701f0df300e27deabe5
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-13 17:27:30 +0545

    Add homepage

--- index.php
+++ index.php
@@ -2,15 +2,55 @@
 <?php include'includes/header.php'; ?>
 
 <head>
-    <title>Home</title>
+    <title>Home - Chautari</title>
+    <link rel="stylesheet" href="./assets/css/default.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <style>
+        nav ul li:nth-child(1) a {
+            background-color: #f0f0f0;
+            padding: 8px 15px;
+            border-radius: 20px;
+        }
+    </style>
 </head>
 <body>
     <main>
-        <h1>Hello World</h1>
+        <h1>
+            Struggling to discover fun and exciting events near you?
+<!--
+            Finding it hard to discover fun and exciting events nearby?
+            
+            Having trouble finding fun and exciting events near you.
+            
+            Struggling to find fun & exiciting events near you?
+            
+            <span id="primary-highlight">fun &&emsp; <span id="smalltext">&ensp;</span>exciting</span> events near you?
+-->
+        </h1>
+        <p class="description">Chautari brings the best local events tailored to you so you can join
+            local happenings with ease.</p>
 
-        <?php include'templates/home.php'; ?>
+        <div class="cta">
+            <a class="primary-button" href="./pages/explore.php">Discover events near you
+                <span class="button-arrow">
+                    <svg width="20" height="20" fill="none" viewBox="0 0 24 24"
+                    class="ml-1">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2.5" d="M13.75 6.75L19.25 12L13.75 17.25"></path>
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2.5" d="M19 12H4.75"></path>
+                    </svg>
+                </span>
+            </a>
+            <a class="secondary-button" href="./pages/about.php">Learn more</a>
+        </div>
 
+        <img class="advert-video" src="./assets/images/placeholder-video.png" alt="video showcasing the product">
     </main>
+
+    <hr>
+    
+    <?php include'templates/home.php'; ?>
 </body>
 </html>
 
